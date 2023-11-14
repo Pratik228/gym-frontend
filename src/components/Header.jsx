05284c06c18ee411c58bfacdf744496a0c2451d9
Sahaj Kedia <sahajkedia20152498@gmail.com>
2023-11-14 13:17:30 +0530

    cart display button change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,137 +15,137 @@ import { toast } from "react-toastify";
 /* Refactor Later
  */
 const Header = () => {
-  const { cartItems } = useSelector((state) => state.cart);
-  const { userInfo } = useSelector((state) => state.auth);
+	const { cartItems } = useSelector((state) => state.cart);
+	const { userInfo } = useSelector((state) => state.auth);
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
-  const [logoutApiCall] = useLogoutMutation();
+	const [logoutApiCall] = useLogoutMutation();
 
-  const logoutHandler = async () => {
-    try {
-      await logoutApiCall().unwrap();
-      dispatch(logout());
-      dispatch(resetCart());
-      toast("Logged out successfully!", { type: "success" });
-      dispatch(
-        showSnackbar({
-          message: "Logged out successfully!",
-          severity: "success",
-        })
-      );
-      navigate("/login");
-    } catch (err) {
-      console.error(err);
-    }
-  };
+	const logoutHandler = async () => {
+		try {
+			await logoutApiCall().unwrap();
+			dispatch(logout());
+			dispatch(resetCart());
+			toast("Logged out successfully!", { type: "success" });
+			dispatch(
+				showSnackbar({
+					message: "Logged out successfully!",
+					severity: "success",
+				})
+			);
+			navigate("/login");
+		} catch (err) {
+			console.error(err);
+		}
+	};
 
-  const itemCount = () => {
-    let count = 0;
-    cartItems.map((item) => {
-      count += item.qty;
-    });
-    return count;
-  };
+	const itemCount = () => {
+		let count = 0;
+		cartItems.map((item) => {
+			count += item.qty;
+		});
+		return count;
+	};
 
-  return (
-    <div className="bg-gray-800 shadow-md mb-8">
-      <div className="container mx-auto flex items-center justify-between">
-        <div className="flex items-center">
-          <img
-            src="/img/Logo.png"
-            alt="MinimalAura Logo"
-            className="w-24 h-24"
-          />
-          <div className="text-2xl font-semibold">MinimalAura</div>
-        </div>
-        <div className="flex space-x-6 items-center">
-          <NavLink
-            to="/"
-            className={({ isActive }) =>
-              isActive
-                ? "text-indigo-400 hover:text-indigo-500"
-                : "hover:text-indigo-500"
-            }
-          >
-            Home
-          </NavLink>
-          <NavLink
-            to="/products"
-            className={({ isActive }) =>
-              isActive
-                ? "text-indigo-400 hover:text-indigo-500"
-                : "hover:text-indigo-500"
-            }
-          >
-            Products
-          </NavLink>
-          {userInfo && (
-            <NavLink
-              to="/profile"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-indigo-400 hover:text-indigo-500"
-                  : "hover:text-indigo-500"
-              }
-            >
-              Profile
-            </NavLink>
-          )}
-          <NavLink
-            to="/cart"
-            className={({ isActive }) =>
-              isActive
-                ? "text-indigo-400 hover:text-indigo-500"
-                : "hover:text-indigo-500"
-            }
-          >
-            <span className="mr-2"> Cart {itemCount() ? itemCount() : ""}</span>
-            <ShoppingCartIcon className="hover:text-indigo-500 cursor-pointer" />
-          </NavLink>
+	return (
+		<div className="bg-gray-800 shadow-md mb-8">
+			<div className="container mx-auto flex items-center justify-between">
+				<div className="flex items-center">
+					<img
+						src="/img/Logo.png"
+						alt="MinimalAura Logo"
+						className="w-24 h-24"
+					/>
+					<div className="text-2xl font-semibold">MinimalAura</div>
+				</div>
+				<div className="flex space-x-6 items-center">
+					<NavLink
+						to="/"
+						className={({ isActive }) =>
+							isActive
+								? "text-indigo-400 hover:text-indigo-500"
+								: "hover:text-indigo-500"
+						}>
+						Home
+					</NavLink>
+					<NavLink
+						to="/products"
+						className={({ isActive }) =>
+							isActive
+								? "text-indigo-400 hover:text-indigo-500"
+								: "hover:text-indigo-500"
+						}>
+						Products
+					</NavLink>
+					{userInfo && (
+						<NavLink
+							to="/profile"
+							className={({ isActive }) =>
+								isActive
+									? "text-indigo-400 hover:text-indigo-500"
+									: "hover:text-indigo-500"
+							}>
+							Profile
+						</NavLink>
+					)}
+					<NavLink
+						to="/cart"
+						className={({ isActive }) =>
+							isActive
+								? "text-indigo-400 hover:text-indigo-500"
+								: "hover:text-indigo-500"
+						}>
+						<div className="relative inline-flex items-center">
+							<ShoppingCartIcon className="hover:text-indigo-500 cursor-pointer" />
+							{itemCount() > 0 && (
+								<span className="absolute -top-4 -right-1 bg-red-500 text-white text-xs rounded-full h-5 w-5 flex items-center justify-center">
+									{itemCount()}
+								</span>
+							)}
+							<span className="ml-2">Cart</span>
+						</div>
+					</NavLink>
+					<NavLink
+						to="/customer-service"
+						className={({ isActive }) =>
+							isActive
+								? "text-indigo-400 hover:text-indigo-500"
+								: "hover:text-indigo-500"
+						}>
+						<SupportAgentIcon className="hover:text-indigo-500 mr-2  cursor-pointer" />
+						<span className="mr-2">Service</span>
+					</NavLink>
 
-          <NavLink
-            to="/customer-service"
-            className={({ isActive }) =>
-              isActive
-                ? "text-indigo-400 hover:text-indigo-500"
-                : "hover:text-indigo-500"
-            }
-          >
-            <SupportAgentIcon className="hover:text-indigo-500 mr-2  cursor-pointer" />
-            <span className="mr-2">Service</span>
-          </NavLink>
-
-          {userInfo ? (
-            <>
-              <div className="flex items-center space-x-2">
-                <AccountCircleIcon className="hover:text-indigo-500 cursor-pointer" />
-                <div className="hover:text-indigo-500 cursor-pointer">
-                  {userInfo.name}
-                </div>
-              </div>
-              <LogoutIcon
-                className="hover:text-indigo-500 cursor-pointer"
-                onClick={logoutHandler}
-              />
-            </>
-          ) : (
-            <NavLink
-              to="/login"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-indigo-400 hover:text-indigo-500"
-                  : "hover:text-indigo-500"
-              }
-            >
-              Login
-            </NavLink>
-          )}
-        </div>
-      </div>
-    </div>
-  );
+					{userInfo ? (
+						<>
+							<div className="flex items-center space-x-2">
+								<AccountCircleIcon className="hover:text-indigo-500 cursor-pointer" />
+								<div className="hover:text-indigo-500 cursor-pointer">
+									{userInfo.name}
+								</div>
+							</div>
+							<LogoutIcon
+								className="hover:text-indigo-500 cursor-pointer"
+								onClick={logoutHandler}
+							/>
+						</>
+					) : (
+						<NavLink
+							to="/login"
+							className={({ isActive }) =>
+								isActive
+									? "text-indigo-400 hover:text-indigo-500"
+									: "hover:text-indigo-500"
+							}>
+							Login
+						</NavLink>
+					)}
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Header;
