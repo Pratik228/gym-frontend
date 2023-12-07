a62e1c03d1524aa0370dd4555594040c2aadeb21
Sahaj Kedia <sahajkedia20152498@gmail.com>
2023-12-08 00:14:33 +0530

    add faqs, customer-service screens

--- src/screens/CustomerService.jsx
+++ src/screens/CustomerService.jsx
@@ -9,170 +9,194 @@ import {
 } from "@mui/material";
 import Modal from "react-modal";
 
-const customStyles = {
-	content: {
-		top: "50%",
-		left: "50%",
-		right: "auto",
-		bottom: "auto",
-		marginRight: "-50%",
-		transform: "translate(-50%, -50%)",
-		border: "none",
-		padding: 0,
-		overflow: "visible",
-	},
-	overlay: {
-		backgroundColor: "rgba(0, 0, 0, 0.75)",
-	},
-};
-
+import { useSelector, useDispatch } from "react-redux";
+import { showSnackbar } from "../slices/snackbarSlice";
+import {
+	useGetOrdersQuery,
+	useCancelOrderMutation,
+	useShipOrderMutation,
+	useDeliverOrderMutation,
+} from "../slices/ordersApiSlice";
 const issues = [
 	{
-		title: "Issue 1",
+		id: 1,
+		number: "1001",
 		email: "user1@example.com",
-		reply: "Reply 1",
+		description: "Issue description 1",
 		status: "Open",
-		additionalInfo: "Info 1",
 	},
 	{
-		title: "Issue 2",
+		id: 2,
+		number: "1002",
 		email: "user2@example.com",
-		reply: "Reply 2",
-		status: "Closed",
-		additionalInfo: "Info 2",
+		description: "Issue description 2",
+		status: "In Progress",
 	},
+	{
+		id: 3,
+		number: "1003",
+		email: "user3@example.com",
+		description: "Issue description 3",
+		status: "Resolved",
+	},
+	// ... add more issues as needed
 ];
 
 const CustomerService = () => {
-	const getStatusStyle = (status) => {
-		switch (status.toLowerCase()) {
-			case "open":
-				return "text-red-500 font-semibold";
-			case "closed":
-				return "text-green-500 font-semibold";
-			default:
-				return "text-gray-500 font-semibold";
-		}
-	};
-	const [showModal, setShowModal] = useState(false);
-	const openModal = (index = null) => {
-		setShowModal(true);
+	const { data: orders, isLoading, error, refetch } = useGetOrdersQuery();
+	const [cancelOrder] = useCancelOrderMutation();
+	const [shipOrder] = useShipOrderMutation();
+	const [deliverOrder] = useDeliverOrderMutation();
+	const dispatch = useDispatch();
+	const [modalIsOpen, setModalIsOpen] = useState(false);
+	const [currentIssue, setCurrentIssue] = useState(null);
+	const [replyMessage, setReplyMessage] = useState("");
+
+	const openModal = (issue) => {
+		setCurrentIssue(issue);
+		setModalIsOpen(true);
 	};
 
 	const closeModal = () => {
-		setShowModal(false);
-		setCurrentAddress(null);
+		setModalIsOpen(false);
+		setCurrentIssue(null);
+		setReplyMessage("");
 	};
 
-	const onClickHandler = () => {};
-	return (
-		<div>
-			<TableContainer className="m-4 border border-gray-200 max-w-full">
-				<Table className="min-w-full divide-y divide-gray-200">
-					<thead>
-						<TableRow>
-							<TableCell className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-								Issue
-							</TableCell>
-							<TableCell className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-								Email
-							</TableCell>
-							<TableCell className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-								Reply
-							</TableCell>
-							<TableCell className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-								Status
-							</TableCell>
-							<TableCell className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-								Additional Column
-							</TableCell>
-						</TableRow>
-					</thead>
-					<TableBody className="divide-y divide-gray-200">
-						{issues.map((issue, index) => (
-							<TableRow key={index}>
-								<TableCell className="px-6 py-4 whitespace-nowrap">
-									{issue.title}
-								</TableCell>
-								<TableCell className="px-6 py-4 whitespace-nowrap">
-									{issue.email}
-								</TableCell>
-								<TableCell className="px-6 py-4 whitespace-nowrap">
-									<button
-										className="text-indigo-600 hover:text-indigo-900"
-										onClick={openModal}>
-										Reply
-									</button>
-								</TableCell>
-								<TableCell
-									className={`px-6 py-4 whitespace-nowrap ${getStatusStyle(
-										issue.status
-									)}`}>
-									{issue.status}
-								</TableCell>
-								<TableCell className="px-6 py-4 whitespace-nowrap">
-									{issue.additionalInfo}
-								</TableCell>
-							</TableRow>
-						))}
-					</TableBody>
-				</Table>
-			</TableContainer>
-			<Modal
-				isOpen={showModal}
-				onRequestClose={closeModal}>
-				<div className="relative bg-gray-800 p-4 max-h-full">
-					<h3 className="text-lg font-semibold text-white mb-4">
-						Reply to the email
-					</h3>
-					<div className="flex flex-wrap mx-3 mb-6">
-						<div className="w-full  px-3 mb-6">
-							<label className="block uppercase tracking-wide text-xs font-bold mb-2">
-								Issue ID
-							</label>
-							<input
-								className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-1 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-								name="issue"
-								type="text"
-								placeholder="Issue #"
-							/>
-						</div>
-						<div className="w-full px-3 mb-6">
-							<label className="block uppercase tracking-wide text-xs font-bold mb-2">
-								Email
-							</label>
-							<input
-								className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-2 px-2 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-								id="email"
-								type="text"
-								placeholder="john@example.com"
-							/>
-						</div>
-						<div className="w-full  px-3 mb-6">
-							<label className="block uppercase tracking-wide text-xs font-bold mb-2">
-								Message
-							</label>
-							<input
-								className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-4 px-8 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
-								name="message"
-								type="text"
-								placeholder="Write your message here"
-							/>
-						</div>
-					</div>
-				</div>
+	const sendReply = () => {
+		console.log(
+			"Replying to issue:",
+			currentIssue?.number,
+			"with message:",
+			replyMessage
+		);
+		// Here you can call an API to send the message
+		closeModal(); // Close the modal after sending the message
+	};
 
-				<div className="relative bg-gray-800 p-4 w-full max-h-full">
-					<div className="flex gap-2">
-						<button className="btn-primary">Send</button>
-						<button
-							onClick={closeModal}
-							className="btn-primary">
-							Cancel
-						</button>
+	// const [updateOrderStatus] = useUpdateOrderStatusMutation();
+
+	const { userInfo } = useSelector((state) => state.auth);
+	const handleStatusChange = async (orderId, newStatus) => {
+		console.log(orderId, newStatus);
+		try {
+			if (newStatus === "Cancelled") {
+				await cancelOrder(orderId).unwrap();
+			} else if (newStatus === "Shipped") {
+				await shipOrder(orderId).unwrap();
+			} else if (newStatus === "Delivered") {
+				await deliverOrder(orderId).unwrap();
+			}
+			dispatch(
+				showSnackbar({
+					message: `Order ${newStatus.toLowerCase()} successfully.`,
+					severity: "success",
+				})
+			);
+			refetch();
+		} catch (err) {
+			dispatch(
+				showSnackbar({
+					message: err.data ? err.data.message : "Error updating order status",
+					severity: "error",
+				})
+			);
+		}
+	};
+
+	if (isLoading) return <div>Loading...</div>;
+	if (error) return <div>Error: {error.message}</div>;
+
+	return (
+		<div className="container mx-auto px-4 sm:px-6 lg:px-8">
+			<div className="flex flex-wrap -mx-4">
+				<div className="w-full px-4">
+					<h1 className="text-xl font-semibold mb-4">Order Management</h1>
+
+					<Modal
+						isOpen={modalIsOpen}
+						onRequestClose={closeModal}
+						contentLabel="Reply Message"
+						// className="modal-class" // Add your modal classes here
+						// overlayClassName="overlay-class" // Add your overlay classes here
+					>
+						<div className="p-4">
+							<h2 className="text-lg font-semibold mb-4">
+								Reply to Issue #{currentIssue?.number}
+							</h2>
+							<textarea
+								value={replyMessage}
+								onChange={(e) => setReplyMessage(e.target.value)}
+								placeholder="Message"
+								className="w-full p-2 border border-gray-300 rounded"
+							/>
+							<div className="flex justify-end mt-4">
+								<button
+									onClick={sendReply}
+									className="btn-primary">
+									Send
+								</button>
+								<button
+									onClick={closeModal}
+									className="btn-primary">
+									Close
+								</button>
+							</div>
+						</div>
+					</Modal>
+
+					{/* Orders List */}
+					<div className="bg-gray-800 shadow overflow-hidden sm:rounded-lg mb-4">
+						<table className="min-w-full divide-y divide-gray-200">
+							<thead className="bg-gray-700">
+								<tr>
+									<th className="px-6 py-3 text-left text-xs font-medium text-gray-200 w-1/6">
+										Issue
+									</th>
+									<th className="px-6 py-3 text-left text-xs font-medium text-gray-200 w-1/6">
+										Email
+									</th>
+									<th className="px-6 py-3 text-left text-xs font-medium text-gray-200 w-1/6">
+										Description
+									</th>
+									<th className="px-6 py-3 text-left text-xs font-medium text-gray-200 w-1/6">
+										Status
+									</th>
+									<th className="px-6 py-3 text-left text-xs font-medium text-gray-200 w-1/6">
+										Reply
+									</th>
+								</tr>
+							</thead>
+							<tbody className="bg-gray-700 divide-y divide-gray-600">
+								{issues.map((issue) => (
+									<tr key={issue.id}>
+										<td className="px-6 py-4 whitespace-nowrap text-left text-sm text-white">
+											#{issue.number}
+										</td>
+										<td className="px-6 py-4 whitespace-nowrap text-left text-sm text-white">
+											{issue.email}
+										</td>
+										<td className="px-6 py-4 whitespace-nowrap text-left text-sm text-white">
+											{issue.description}
+										</td>
+										<td className="px-6 py-4 whitespace-nowrap text-left text-sm text-white">
+											${issue.status}
+										</td>
+
+										<td className="px-6 py-4 whitespace-nowrap text-left text-sm font-medium">
+											<button
+												onClick={() => openModal(issue)}
+												className="text-blue-400 hover:text-blue-500 mr-2">
+												Reply
+											</button>
+										</td>
+									</tr>
+								))}
+							</tbody>
+						</table>
 					</div>
 				</div>
-			</Modal>
+			</div>
 		</div>
 	);
 };
