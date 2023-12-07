be25d39528d631368e592207e660a2bbf57a34f6
Sahaj Kedia <sahajkedia20152498@gmail.com>
2023-12-08 00:17:19 +0530

    add Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,210 +19,206 @@ import { toast } from "react-toastify";
 /* Refactor Later
  */
 const Header = () => {
-  const { cartItems } = useSelector((state) => state.cart);
-  const { userInfo } = useSelector((state) => state.auth);
+	const { cartItems } = useSelector((state) => state.cart);
+	const { userInfo } = useSelector((state) => state.auth);
 
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
 
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  const [logoutApiCall] = useLogoutMutation();
+	const [logoutApiCall] = useLogoutMutation();
 
-  const logoutHandler = async () => {
-    try {
-      await logoutApiCall().unwrap();
-      dispatch(logout());
-      dispatch(resetCart());
-      dispatch(
-        showSnackbar({
-          message: "Logged out successfully!",
-          severity: "success",
-        })
-      );
-      navigate("/login");
-    } catch (err) {
-      console.error(err);
-    }
-  };
+	const logoutHandler = async () => {
+		try {
+			await logoutApiCall().unwrap();
+			dispatch(logout());
+			dispatch(resetCart());
+			dispatch(
+				showSnackbar({
+					message: "Logged out successfully!",
+					severity: "success",
+				})
+			);
+			navigate("/login");
+		} catch (err) {
+			console.error(err);
+		}
+	};
 
-  const itemCount = () => {
-    let count = 0;
-    cartItems.map((item) => {
-      count += item.qty;
-    });
-    return count;
-  };
+	const itemCount = () => {
+		let count = 0;
+		cartItems.map((item) => {
+			count += item.qty;
+		});
+		return count;
+	};
 
-  return (
-    <div className="bg-gray-800 shadow-md mb-8">
-      <div className="container mx-auto flex items-center justify-between">
-        <div className="flex items-center">
-          <img
-            src="/img/Logo.png"
-            alt="MinimalAura Logo"
-            className="w-24 h-24"
-          />
-          <div className="text-2xl font-semibold">MinimalAura</div>
-        </div>
-        {userInfo && userInfo.isAdmin === 1 && (
-          <div className="flex space-x-6 items-center">
-            <NavLink
-              to="/admin/productlist"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-indigo-400 hover:text-indigo-500"
-                  : "hover:text-indigo-500"
-              }
-            >
-              Products
-            </NavLink>
-            <NavLink
-              to="/admin/orderlist"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-indigo-400 hover:text-indigo-500"
-                  : "hover:text-indigo-500"
-              }
-            >
-              Orders
-            </NavLink>
-            <NavLink
-              to="/admin/customer-service"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-indigo-400 hover:text-indigo-500"
-                  : "hover:text-indigo-500"
-              }
-            >
-              Service
-            </NavLink>
-            <div className="flex items-center cursor-pointer">
-              <AccountCircleIcon className="text-white" />
-              <span className="text-white ml-2">{userInfo.name}</span>
-            </div>
-          </div>
-        )}
+	return (
+		<div className="bg-gray-800 shadow-md mb-8">
+			<div className="container mx-auto flex items-center justify-between">
+				<div className="flex items-center">
+					<img
+						src="/img/Logo.png"
+						alt="MinimalAura Logo"
+						className="w-24 h-24"
+					/>
+					<div className="text-2xl font-semibold">MinimalAura</div>
+				</div>
+				{userInfo && userInfo.isAdmin === 1 && (
+					<div className="flex space-x-6 items-center">
+						<NavLink
+							to="/admin/productlist"
+							className={({ isActive }) =>
+								isActive
+									? "text-indigo-400 hover:text-indigo-500"
+									: "hover:text-indigo-500"
+							}>
+							Products
+						</NavLink>
+						<NavLink
+							to="/admin/orderlist"
+							className={({ isActive }) =>
+								isActive
+									? "text-indigo-400 hover:text-indigo-500"
+									: "hover:text-indigo-500"
+							}>
+							Orders
+						</NavLink>
+						<NavLink
+							to="/admin/customer-service"
+							className={({ isActive }) =>
+								isActive
+									? "text-indigo-400 hover:text-indigo-500"
+									: "hover:text-indigo-500"
+							}>
+							Service
+						</NavLink>
+						<div className="flex items-center cursor-pointer">
+							<AccountCircleIcon className="text-white" />
+							<span className="text-white ml-2">{userInfo.name}</span>
+						</div>
+					</div>
+				)}
 
-        {userInfo && userInfo.isAdmin === 0 && (
-          <div className="flex space-x-6 items-center">
-            <NavLink
-              to="/"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-indigo-400 hover:text-indigo-500"
-                  : "hover:text-indigo-500"
-              }
-            >
-              Home
-            </NavLink>
-            <NavLink
-              to="/products"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-indigo-400 hover:text-indigo-500"
-                  : "hover:text-indigo-500"
-              }
-            >
-              Products
-            </NavLink>
-            {userInfo && (
-              <NavLink
-                to="/profile"
-                className={({ isActive }) =>
-                  isActive
-                    ? "text-indigo-400 hover:text-indigo-500"
-                    : "hover:text-indigo-500"
-                }
-              >
-                Profile
-              </NavLink>
-            )}
-            <NavLink
-              to="/cart"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-indigo-400 hover:text-indigo-500"
-                  : "hover:text-indigo-500"
-              }
-            >
-              <div className="flex items-center space-x-3">
-                <Badge
-                  badgeContent={itemCount()}
-                  color="primary"
-                  overlap="circular"
-                >
-                  <ShoppingCartIcon className="hover:text-indigo-500 cursor-pointer" />
-                </Badge>
-                <span className="ml-2">Cart</span>
-              </div>
-            </NavLink>
-            <NavLink
-              to="/customer-service"
-              className={({ isActive }) =>
-                isActive
-                  ? "text-indigo-400 hover:text-indigo-500"
-                  : "hover:text-indigo-500"
-              }
-            >
-              <SupportAgentIcon className="hover:text-indigo-500 mr-2  cursor-pointer" />
-              <span className="mr-2">Service</span>
-            </NavLink>
-            {userInfo ? (
-              <>
-                <div className="relative group">
-                  <div className="flex items-center cursor-pointer">
-                    <AccountCircleIcon className="text-white" />
-                    <span className="text-white ml-2">{userInfo.name}</span>
-                  </div>
-                  <div className="absolute group-hover:block dropdown-menu hidden right-0 w-36 bg-gray-700 rounded-md shadow-xl z-20 transition-opacity duration-300 ease-in-out group-hover:opacity-100">
-                    <ul className="py-2">
-                      <li>
-                        <NavLink
-                          to="/profile"
-                          className="block px-4 py-2 hover:bg-gray-800 text-white"
-                        >
-                          My Profile
-                        </NavLink>
-                      </li>
-                      <li>
-                        <NavLink
-                          to="/my-orders"
-                          className="block px-4 py-2 hover:bg-gray-800 text-white"
-                        >
-                          My Orders
-                        </NavLink>
-                      </li>
-                      <li className="py-1">
-                        <hr className="border-t border-gray-600" />
-                        <span
-                          onClick={logoutHandler}
-                          className="block px-4 py-2 hover:bg-gray-800 text-white cursor-pointer"
-                        >
-                          Logout
-                        </span>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              </>
-            ) : (
-              <NavLink
-                to="/login"
-                className={({ isActive }) =>
-                  isActive
-                    ? "text-indigo-400 hover:text-indigo-500"
-                    : "hover:text-indigo-500"
-                }
-              >
-                Login
-              </NavLink>
-            )}
-          </div>
-        )}
-      </div>
-    </div>
-  );
+				{userInfo && userInfo.isAdmin === 0 && (
+					<div className="flex space-x-6 items-center">
+						<NavLink
+							to="/"
+							className={({ isActive }) =>
+								isActive
+									? "text-indigo-400 hover:text-indigo-500"
+									: "hover:text-indigo-500"
+							}>
+							Home
+						</NavLink>
+						<NavLink
+							to="/products"
+							className={({ isActive }) =>
+								isActive
+									? "text-indigo-400 hover:text-indigo-500"
+									: "hover:text-indigo-500"
+							}>
+							Products
+						</NavLink>
+						<NavLink
+							to="/faqs"
+							className={({ isActive }) =>
+								isActive
+									? "text-indigo-400 hover:text-indigo-500"
+									: "hover:text-indigo-500"
+							}>
+							FAQ
+						</NavLink>
+						{userInfo && (
+							<NavLink
+								to="/profile"
+								className={({ isActive }) =>
+									isActive
+										? "text-indigo-400 hover:text-indigo-500"
+										: "hover:text-indigo-500"
+								}>
+								Profile
+							</NavLink>
+						)}
+						<NavLink
+							to="/cart"
+							className={({ isActive }) =>
+								isActive
+									? "text-indigo-400 hover:text-indigo-500"
+									: "hover:text-indigo-500"
+							}>
+							<div className="flex items-center space-x-3">
+								<Badge
+									badgeContent={itemCount()}
+									color="primary"
+									overlap="circular">
+									<ShoppingCartIcon className="hover:text-indigo-500 cursor-pointer" />
+								</Badge>
+								<span className="ml-2">Cart</span>
+							</div>
+						</NavLink>
+						<NavLink
+							to="/customer-service"
+							className={({ isActive }) =>
+								isActive
+									? "text-indigo-400 hover:text-indigo-500"
+									: "hover:text-indigo-500"
+							}>
+							<SupportAgentIcon className="hover:text-indigo-500 mr-2  cursor-pointer" />
+							<span className="mr-2">Service</span>
+						</NavLink>
+						{userInfo ? (
+							<>
+								<div className="relative group">
+									<div className="flex items-center cursor-pointer">
+										<AccountCircleIcon className="text-white" />
+										<span className="text-white ml-2">{userInfo.name}</span>
+									</div>
+									<div className="absolute group-hover:block dropdown-menu hidden right-0 w-36 bg-gray-700 rounded-md shadow-xl z-20 transition-opacity duration-300 ease-in-out group-hover:opacity-100">
+										<ul className="py-2">
+											<li>
+												<NavLink
+													to="/profile"
+													className="block px-4 py-2 hover:bg-gray-800 text-white">
+													My Profile
+												</NavLink>
+											</li>
+											<li>
+												<NavLink
+													to="/my-orders"
+													className="block px-4 py-2 hover:bg-gray-800 text-white">
+													My Orders
+												</NavLink>
+											</li>
+											<li className="py-1">
+												<hr className="border-t border-gray-600" />
+												<span
+													onClick={logoutHandler}
+													className="block px-4 py-2 hover:bg-gray-800 text-white cursor-pointer">
+													Logout
+												</span>
+											</li>
+										</ul>
+									</div>
+								</div>
+							</>
+						) : (
+							<NavLink
+								to="/login"
+								className={({ isActive }) =>
+									isActive
+										? "text-indigo-400 hover:text-indigo-500"
+										: "hover:text-indigo-500"
+								}>
+								Login
+							</NavLink>
+						)}
+					</div>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default Header;
