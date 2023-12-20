d7f7ac476b7da915da6f6287a4332706fa3f6fec
Sachin Kumar <sachinkumar69@deloitte.com>
2023-12-20 22:17:59 +0530

    Add build folder

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://3.91.202.182:5000";
+export const BASE_URL = "http://107.23.159.142:5000";
 export const PRODUCTS_URL = "/api/products";
 // export const PRODUCTS_URL = "https://fakestoreapi.com/products";
 export const USERS_URL = "/api/users";
